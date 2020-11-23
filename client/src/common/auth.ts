350b378099ac805fa78f3be4d9d7e8fe619b715c
Jake Ploskey <jakeploskey@gmail.com>
2020-11-22 23:15:01 -0800

    Move auth graphql queries to separate file

--- client/src/common/auth.ts
+++ client/src/common/auth.ts
@@ -1,53 +1,6 @@
 import { UUID } from "@amble/common/uuid"
-import { gql } from "@apollo/client"
 import _ from "lodash"
 import { useMeQuery } from "../generated/graphql"
-
-gql`
-  mutation CreateUser($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password) {
-      id
-      username
-      email
-    }
-  }
-
-  mutation Login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      user {
-        id
-        username
-        email
-      }
-      accessToken
-    }
-  }
-
-  mutation Refresh {
-    refresh {
-      user {
-        id
-        username
-        email
-      }
-      accessToken
-    }
-  }
-
-  mutation Logout {
-    logout {
-      success
-    }
-  }
-
-  query ME {
-    me {
-      id
-      username
-      email
-    }
-  }
-`
 
 export type UserInfo = Readonly<{
   id: UUID
